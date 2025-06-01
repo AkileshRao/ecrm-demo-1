149e55939f5497b1e488ac79a8c9deba85933869
AkileshRao <akileshrao19@gmail.com>
2025-06-01 11:53:17 +0530

    fix: updated page title

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -8,7 +8,7 @@ const ListingPage = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Apple Store Products</h1>
+      <h1 className="text-2xl font-bold mb-4">Apple Store Products Page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4">
         {products.map((product) => (
           <Link to={`/cart/${product.id}`} key={product.id}>
